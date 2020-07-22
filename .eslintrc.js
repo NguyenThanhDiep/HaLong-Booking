ab08f9c216fe1c23bb64369e9e9de2d4940a3d67
Diep Nguyen <nguyenthanhdiep21051995@gmail.com>
2020-07-22 21:48:05 +0700

    Finished config webapp

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,9 @@ module.exports = {
     rules: {
       // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
       // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/explicit-function-return-type": "off"
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     }
   };
