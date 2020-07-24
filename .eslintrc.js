71ef105f97531a70983ef65332cb24c99a3d97f6
Diep-NGUYEN <thanh-diep.nguyen@bollore.com>
2020-07-24 16:44:47 +0700

    Fix config eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
     parserOptions: {
       ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-      sourceType: "module" // Allows for the use of imports
+      sourceType: "module", // Allows for the use of imports
+      parser: "@typescript-eslint/parser" // Specifies the ESLint parser
     },
     extends: [
-      "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+      "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+      "plugin:vue/recommended",
+      "plugin:vue/base"
     ],
     rules: {
       // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
@@ -13,6 +15,7 @@ module.exports = {
       "@typescript-eslint/no-inferrable-types": "off",
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "vue/html-self-closing": "off"
     }
   };
