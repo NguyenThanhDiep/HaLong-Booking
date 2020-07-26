63cb8432ef9ddcc4e2b9ada43c9281a68dd6cd33
Diep Nguyen <nguyenthanhdiep21051995@gmail.com>
2020-07-26 16:39:20 +0700

    Hotel page model

--- src/components/Hotel/hotel.component.ts
+++ src/components/Hotel/hotel.component.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import Hotel from '@/models/Hotel';
+import Hotel, { HotelMockData, FreeService, ServiceHotel, StarHotel } from '@/models/Hotel';
 import SearchComponent from '@/components/Shared/Search/search.component.vue';
 
 @Component({
@@ -10,5 +10,15 @@ export default class HotelComponent extends Vue {
     hotelsOrigin: Array<Hotel> = [];
     hotelsFiltered: Array<Hotel> = [];
 
+    filterStars: Array<any> = [];
+    starHotel: Array<number> = StarHotel;
+    filterFreeServices: Array<any> = [];
+    freeServiceHotel = FreeService;
+    filerServices: Array<any> = [];
+    serviceHotel = ServiceHotel;
+
     //-----Method-----//
+    mounted() {
+        this.hotelsOrigin = this.hotelsFiltered = HotelMockData;
+    }
 }
--- src/models/Hotel.ts
+++ src/models/Hotel.ts
@@ -4,17 +4,33 @@ export default class Hotel {
     price: number = 0;
     star: number = 0;
     address: string = '';
-    service: Array<{ code: string; name: string}> = [];
+    freeServices: Array<{ code: string; name: string}> = [];
+    services: Array<{ code: string; name: string }> = [];
+    isShowAll: boolean = false;
     
     constructor(name: string, scrImg: string, price: number, star: number = 0,
-        address: string = '', service: Array<{ code: string; name: string }> = []) {
+        address: string = '', freeServices: Array<{ code: string; name: string }> = [],
+        services: Array<{ code: string; name: string }> = []) {
         this.name = name;
         this.srcImg = scrImg;
         this.price = price;
         this.star = star;
         this.address = address;
-        this.service = service;
+        this.freeServices = freeServices;
+        this.services = services;
     }
+
+    get totalServises(): number {
+        return this.freeServices.length + this.services.length;
+    }
+}
+
+export const StarHotel = [1, 2, 3, 4, 5]
+
+export const FreeService = {
+    FreeAnSang: { code: 'FreeAnSang', name: 'Bữa sáng miễn phí' },
+    Free3Bua: { code: 'Free3Bua', name: '3 bữa ăn miễn phí' },
+    ThemGiuongPhu: { code: 'ThemGiuongPhu', name: 'Thêm giường phụ' }
 }
 
 export const ServiceHotel = {
@@ -38,33 +54,43 @@ export const ServiceHotel = {
 export const HotelMockData = [
     new Hotel('Novotel Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 1300000, 1,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.FreeAnSang],
         [ServiceHotel.BaiDoXe, ServiceHotel.ChapNhanThuCung, ServiceHotel.DoAnTaiPhong, ServiceHotel.HoBoi, ServiceHotel.MayATM]),
     new Hotel('Central Luxury Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 1700000, 1,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.Free3Bua],
         [ServiceHotel.BaiDoXe, ServiceHotel.ChapNhanThuCung, ServiceHotel.DoAnTaiPhong, ServiceHotel.HoBoi, ServiceHotel.MayATM]),
     new Hotel('StarCity Hạ Long Bay', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 2300000, 2,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.ThemGiuongPhu],
         [ServiceHotel.BaiDoXe, ServiceHotel.ChapNhanThuCung, ServiceHotel.DoAnTaiPhong, ServiceHotel.HoBoi, ServiceHotel.MayATM]),
     new Hotel('Sài Gòn Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 2500000, 2,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.Free3Bua, FreeService.FreeAnSang],
         [ServiceHotel.HoBoi, ServiceHotel.Massage, ServiceHotel.ThangMay, ServiceHotel.PhongGym, ServiceHotel.GiatLa]),
     new Hotel('Grand Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 3100000, 3,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.ThemGiuongPhu, FreeService.FreeAnSang],
         [ServiceHotel.Wifi, ServiceHotel.ThueXeMay, ServiceHotel.LeTan24, ServiceHotel.ChapNhanThuCung, ServiceHotel.MayATM]),
     new Hotel('Paradise Suites Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 3800000, 3,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.Free3Bua],
         [ServiceHotel.NhaHang, ServiceHotel.DuaDonSanBay, ServiceHotel.BaiDoXe, ServiceHotel.HoTroDatTour, ServiceHotel.DoAnTaiPhong]),
     new Hotel('Mường Thanh Luxury Quảng Ninh', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 4200000, 4,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.FreeAnSang],
         [ServiceHotel.PhongGym, ServiceHotel.ChapNhanThuCung, ServiceHotel.LeTan24, ServiceHotel.BaiDoXe, ServiceHotel.ThangMay]),
     new Hotel('Royal Lotus Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 4500000, 4,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.ThemGiuongPhu],
         [ServiceHotel.ChapNhanThuCung, ServiceHotel.ThueXeMay, ServiceHotel.GiatLa, ServiceHotel.HoBoi, ServiceHotel.Massage]),
     new Hotel('FiveStar Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 4900000, 5,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.Free3Bua],
         [ServiceHotel.BaiDoXe, ServiceHotel.ThueXeMay, ServiceHotel.HoTroDatTour, ServiceHotel.LeTan24, ServiceHotel.DuaDonSanBay]),
     new Hotel('Royal Hạ Long', 'https://q-cf.bstatic.com/images/hotel/max1280x900/688/68867405.jpg', 5000000, 5,
         '160 Đường Hạ Long, Bãi Cháy, Hạ Long',
+        [FreeService.FreeAnSang],
         [ServiceHotel.DuaDonSanBay, ServiceHotel.HoTroDatTour, ServiceHotel.DoAnTaiPhong, ServiceHotel.HoBoi, ServiceHotel.MayATM])
 
 ]
