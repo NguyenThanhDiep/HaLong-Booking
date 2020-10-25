2b421e70de86211fc58e1d734188f613a616bf81
Diep Nguyen <nguyenthanhdiep21051995@gmail.com>
2020-10-25 17:18:21 +0700

    Css experience page

--- src/components/TravelExperience/travelExperience.component.ts
+++ src/components/TravelExperience/travelExperience.component.ts
@@ -1,7 +1,26 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import SearchComponent from '@/components/Shared/Search/search.component.vue';
 
-@Component({})
+@Component({
+    components: { SearchComponent }
+})
 export default class TravelExperienceComponent extends Vue {
 
+    mounted() {
+        this.scrollToTop();
+        const crollToTopBtn = document.getElementById("scrollTopBtn") as HTMLButtonElement;
+        window.onscroll = function () {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                crollToTopBtn.style.display = "block";
+            } else {
+                crollToTopBtn.style.display = "none";
+            }
+        };
+    }
+
+    scrollToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
 }
